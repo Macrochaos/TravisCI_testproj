0f67653a9357de67537602f1940838a586e866d5
Macrochaos <andressumano@hotmail.com>
2018-11-01 16:21:29 -0600

    Lorem ipsum

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 
 int main(){
 	std::cout << 
-	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n
-	Donec at nisl ipsum.\n
-	Praesent malesuada mattis eros, sit amet cursus urna viverra vitae.\n
-	Donec tristique neque ullamcorper, dictum lorem ut, consectetur tortor.\n
-	Fusce accumsan enim ac condimentum vulputate.\n
-	Donec porttitor faucibus efficitur.\n
-	Nulla facilisi.\n
-	Aliquam dictum orci est, et placerat tortor porttitor ut.\n
-	Quisque facilisis vehicula maximus.\n
-	Duis nec tortor tempor, cursus est vitae, pellentesque nisi.\n
-	Curabitur ornare volutpat mollis.\n
-	In consectetur, nulla quis dictum eleifend, eros libero convallis magna, quis pharetra arcu turpis nec ex.\n
-	Quisque quis justo sed enim ullamcorper rhoncus quis eu enim.\n
-	Aliquam sit amet maximus nulla.\n
-	Nunc placerat, magna vitae euismod rhoncus, ipsum magna ultricies tortor, ultrices cursus eros tellus ac leo.\n"; 
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nDonec at nisl ipsum.\nPraesent malesuada mattis eros, sit amet cursus urna viverra vitae.\nDonec tristique neque ullamcorper, dictum lorem ut, consectetur tortor.\nFusce accumsan enim ac condimentum vulputate.\nDonec porttitor faucibus efficitur.\nNulla facilisi.\nAliquam dictum orci est, et placerat tortor porttitor ut.\nQuisque facilisis vehicula maximus.\nDuis nec tortor tempor, cursus est vitae, pellentesque nisi.\nCurabitur ornare volutpat mollis.\nIn consectetur, nulla quis dictum eleifend, eros libero convallis magna, quis pharetra arcu turpis nec ex.\nQuisque quis justo sed enim ullamcorper rhoncus quis eu enim.\nAliquam sit amet maximus nulla.\nNunc placerat, magna vitae euismod rhoncus, ipsum magna ultricies tortor, ultrices cursus eros tellus ac leo.\n"; 
 	return 0;
 }
