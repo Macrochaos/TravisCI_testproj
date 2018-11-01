75b1da4830b5e8f664f92ef3dbfc4028ef781ad0
Macrochaos <andressumano@hotmail.com>
2018-11-01 16:16:22 -0600

    Lorem Ipsum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main(){
-	std::cout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at nisl ipsum.\n Praesent malesuada mattis eros, sit amet cursus urna viverra vitae. Donec tristique neque ullamcorper, dictum lorem ut, consectetur tortor.\n Fusce accumsan enim ac condimentum vulputate.\n Donec porttitor faucibus efficitur.\n Nulla facilisi.\n Aliquam dictum orci est, et placerat tortor porttitor ut.\n Quisque facilisis vehicula maximus.\n Duis nec tortor tempor, cursus est vitae, pellentesque nisi.\n Curabitur ornare volutpat mollis.\n In consectetur, nulla quis dictum eleifend, eros libero convallis magna, quis pharetra arcu turpis nec ex.\n Quisque quis justo sed enim ullamcorper rhoncus quis eu enim.\n Aliquam sit amet maximus nulla.\n Nunc placerat, magna vitae euismod rhoncus, ipsum magna ultricies tortor, ultrices cursus eros tellus ac leo.\n"; 
+	std::cout << 
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n
+	Donec at nisl ipsum.\n
+	Praesent malesuada mattis eros, sit amet cursus urna viverra vitae.\n
+	Donec tristique neque ullamcorper, dictum lorem ut, consectetur tortor.\n
+	Fusce accumsan enim ac condimentum vulputate.\n
+	Donec porttitor faucibus efficitur.\n
+	Nulla facilisi.\n
+	Aliquam dictum orci est, et placerat tortor porttitor ut.\n
+	Quisque facilisis vehicula maximus.\n
+	Duis nec tortor tempor, cursus est vitae, pellentesque nisi.\n
+	Curabitur ornare volutpat mollis.\n
+	In consectetur, nulla quis dictum eleifend, eros libero convallis magna, quis pharetra arcu turpis nec ex.\n
+	Quisque quis justo sed enim ullamcorper rhoncus quis eu enim.\n
+	Aliquam sit amet maximus nulla.\n
+	Nunc placerat, magna vitae euismod rhoncus, ipsum magna ultricies tortor, ultrices cursus eros tellus ac leo.\n"; 
 	return 0;
 }
